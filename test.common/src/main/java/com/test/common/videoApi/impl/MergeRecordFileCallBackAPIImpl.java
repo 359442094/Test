1d25f983b24eb2b0d3474283d82715b1aeef5112
359442094 <359442094@qq.com>
2019-11-18 16:14:25 +0800

    更新合并回调处理

--- test.common/src/main/java/com/test/common/videoApi/impl/MergeRecordFileCallBackAPIImpl.java
+++ test.common/src/main/java/com/test/common/videoApi/impl/MergeRecordFileCallBackAPIImpl.java
@@ -1,16 +1,35 @@
 package com.test.common.videoApi.impl;
 
-import com.test.common.videoApi.MergeRecordFileCallBackAPI;
-import com.test.common.videoApi.MergeRecordFileCallBackRequest;
-import com.test.common.videoApi.MergeRecordFileCallBackResopnse;
+import com.test.common.videoApi.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author CJ
  * @date 2019/10/24
  */
+@Slf4j
 @Component
 public class MergeRecordFileCallBackAPIImpl implements MergeRecordFileCallBackAPI {
+
+    //回调访问前缀
+    @Value(value = "${xiaoYiService.callBackPrefix}")
+    private String callBackPrefix;
+    //同步至点播Url
+    @Value(value = "${xiaoYiService.forwardRecordCallBack}")
+    private String forwardRecordCallBack;
+    //合并视频Url
+    @Value(value = "${xiaoYiService.mergeRecordCallBack}")
+    private String mergeRecordCallBack;
+
+    @Autowired
+    private FileForwardSaveAPI fileForwardSaveAPI;
+
     @Override
     public MergeRecordFileCallBackResopnse process(Integer id,MergeRecordFileCallBackRequest request) {
         MergeRecordFileCallBackResopnse resopnse=new MergeRecordFileCallBackResopnse();
@@ -19,6 +38,21 @@ public class MergeRecordFileCallBackAPIImpl implements MergeRecordFileCallBackAP
         resopnse.setFileIds(request.getFileIds());
         resopnse.setFileName(request.getFileName());
         resopnse.setFileUrl(request.getFileUrl());
+        List<String> fileIds=new ArrayList<>();
+        fileIds.add(request.getFileId());
+        log.info("同步点播上送参数:" + getForwardRequest(id,"383452",fileIds));
+        FileForwardSaveResponse response1 = fileForwardSaveAPI.process(getForwardRequest(id, "383452", fileIds));
+        log.info("转存录制文件到点播结果:" + response1);
         return resopnse;
     }
+
+    public FileForwardSaveRequest getForwardRequest(Integer id, String channelId, List<String> fileIds) {
+        FileForwardSaveRequest forwardRequest = new FileForwardSaveRequest();
+        //回调函数带上录播课程编号
+        forwardRequest.setCallbackUrl(callBackPrefix + forwardRecordCallBack + "?id=" + id);
+        forwardRequest.setChannelId(channelId);
+        forwardRequest.setFileIds(fileIds);
+        return forwardRequest;
+    }
+
 }

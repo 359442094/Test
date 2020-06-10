8073425be8bc0cc1a2e93fe9a78f66ac75d43273
359442094 <359442094@qq.com>
2020-06-10 11:55:15 +0800

    add httpClient upload file

--- test.start/src/main/java/com/test/start/test/util/HttpClientUtil.java
+++ test.start/src/main/java/com/test/start/test/util/HttpClientUtil.java
@@ -2,6 +2,7 @@ package com.test.start.test.util;
 
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -10,6 +11,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
@@ -17,6 +19,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -81,6 +84,53 @@ public class HttpClientUtil {
      */
     public static String doGet(String url) {
         return doGet(url, null);
+    }
+
+    /**
+     * 使用httpclint 发送文件
+     * @author: qingfeng
+     * @date: 2019-05-27
+     * @param file
+     *            上传的文件
+     * @return 响应结果
+     */
+    public static String uploadFile(String url , MultipartFile file, String fileParamName, Map<String,String>headerParams, Map<String,String>otherParams) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String result = "";
+        try {
+            String fileName = file.getOriginalFilename();
+            HttpPost httpPost = new HttpPost(url);
+            //添加header
+            for (Map.Entry<String, String> e : headerParams.entrySet()) {
+                httpPost.addHeader(e.getKey(), e.getValue());
+            }
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setCharset(Charset.forName("utf-8"));
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);//加上此行代码解决返回中文乱码问题
+            builder.addBinaryBody(fileParamName, file.getInputStream(), ContentType.MULTIPART_FORM_DATA, fileName);// 文件流
+            for (Map.Entry<String, String> e : otherParams.entrySet()) {
+                builder.addTextBody(e.getKey(), e.getValue());// 类似浏览器表单提交，对应input的name和value
+            }
+            HttpEntity entity = builder.build();
+            httpPost.setEntity(entity);
+            HttpResponse response = httpClient.execute(httpPost);// 执行提交
+            HttpEntity responseEntity = response.getEntity();
+            if (responseEntity != null) {
+                // 将响应内容转换为字符串
+                result = EntityUtils.toString(responseEntity, Charset.forName("UTF-8"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     /**
@@ -264,4 +314,55 @@ public class HttpClientUtil {
         }
         return resultString;
     }
+
+    /**
+     * 使用httpclint 发送文件
+     * @param file
+     * 调用示例： String json = HttpClientUtil.uploadFile(this.uploadUrl, file, "file", map);
+     *            上传的文件
+     * @return 响应结果
+     */
+    public static String uploadFile(String url , MultipartFile file, String fileParamName,Map<String,String>otherParams) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String result = "";
+        try {
+            String fileName = file.getOriginalFilename();
+            HttpPost httpPost = new HttpPost(url);
+            //添加header
+            /*for (Map.Entry<String, String> e : headerParams.entrySet()) {
+                httpPost.addHeader(e.getKey(), e.getValue());
+            }*/
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setCharset(Charset.forName("utf-8"));
+            //加上此行代码解决返回中文乱码问题
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            // 文件流
+            builder.addBinaryBody(fileParamName, file.getInputStream(), ContentType.MULTIPART_FORM_DATA, fileName+".png");
+            for (Map.Entry<String, String> e : otherParams.entrySet()) {
+                // 类似浏览器表单提交，对应input的name和value
+                builder.addTextBody(e.getKey(), e.getValue());
+            }
+            HttpEntity entity = builder.build();
+            httpPost.setEntity(entity);
+            // 执行提交
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity responseEntity = response.getEntity();
+            if (responseEntity != null) {
+                // 将响应内容转换为字符串
+                result = EntityUtils.toString(responseEntity, Charset.forName("UTF-8"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
 }

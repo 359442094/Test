86bfc7f214f33cb5e3aebb7fee838f8eff69a594
chenjie <359442094@qq.com>
2021-05-20 17:24:51 +0800

    add easyexcel util

--- test.controller/src/main/java/com/test/controller/easyexcel/EasyUser.java
+++ test.controller/src/main/java/com/test/controller/easyexcel/EasyUser.java
@@ -1,7 +1,8 @@
-package com.test.controller;
+package com.test.controller.easyexcel;
 
 import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.alibaba.excel.metadata.BaseRowModel;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,12 +15,15 @@ public class EasyUser implements Serializable {
 
     private static final long serialVersionUID = 509539254519838528L;
 
-    @ExcelProperty("字符串标题")
+    @ColumnWidth(value = 17)
+    @ExcelProperty(value = "字符串标题")
     private String string;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ExcelProperty(value = "日期标题",format = "yyyy-MM-dd HH:mm:ss")
+    @ColumnWidth(value = 20)
     private Date date;
-    @ExcelProperty("数字标题")
+    @ExcelProperty(value = "数字标题")
+    @ColumnWidth(value = 17)
     private Double doubleData;
     /**
      * 忽略这个字段
--- test.controller/src/main/java/com/test/controller/easyexcel/ExcelController.java
+++ test.controller/src/main/java/com/test/controller/easyexcel/ExcelController.java
@@ -1,43 +1,21 @@
-package com.test.controller;
+package com.test.controller.easyexcel;
 
-import cn.afterturn.easypoi.excel.ExcelExportUtil;
-import cn.afterturn.easypoi.excel.entity.ExportParams;
-import com.alibaba.excel.EasyExcel;
-import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.context.AnalysisContext;
-import com.alibaba.excel.event.SyncReadListener;
-import com.alibaba.excel.metadata.CellExtra;
-import com.alibaba.excel.metadata.Sheet;
-import com.alibaba.excel.metadata.Table;
-import com.alibaba.excel.read.listener.ReadListener;
-import com.alibaba.excel.support.ExcelTypeEnum;
-import com.alibaba.excel.write.metadata.WriteSheet;
 import com.test.common.annoation.ShowLogger;
 import com.test.common.util.EasyExcelUtil;
-import com.test.common.util.ExcelUtil;
 import groovy.util.logging.Log4j;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.poi.ss.formula.functions.T;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.jsoup.helper.DataUtil;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.thymeleaf.standard.expression.EachUtils;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @Log4j
 @Api(tags = "excel处理")
